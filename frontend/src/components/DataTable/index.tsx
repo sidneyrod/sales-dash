e47b8b5916b92bd23d0ecd84f0a2454f9073ce10
Sidney Rodrigues <sidney.silva.rodrigues@outlook.com>
2021-05-04 10:21:13 -0300

    Basic static components

---
+++ frontend/src/components/DataTable/index.tsx
@@ -0,0 +1,28 @@
+const DataTable = () => {
+    return (
+        <div className="table-responsive">
+            <table className="table table-striped table-sm">
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Seller</th>
+                        <th>Customers visited</th>
+                        <th>Closed deals</th>
+                        <th>Value</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Barry Allen</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default DataTable;
---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App developed by <a href="https://github.com/sidneyrod" 
+                target="_blank" rel="noreferrer">Sidney Rodrigues</a></p>
+                <p className="text-light"><small><strong>Spring React Week</strong><br />
+                Event promoted by DevSuperior school: <a href="https://instagram.com/devsuperior.ig" 
+                target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
