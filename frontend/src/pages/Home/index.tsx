70e2b53b01d60532179f22329d18f2a3f5167662
Sidney Rodrigues <sidney.silva.rodrigues@outlook.com>
2021-05-07 16:28:22 -0300

    BarChart integration

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -5,19 +5,19 @@ import { Link } from "react-router-dom";
 const Home = () => {
     return (
         <>
-        <Navbar />
-        <div className="container">
-            <div className="jumbotron">
-                <h1 className="display-4">Sales Dash</h1>
-                <p className="lead">Analyze your sales performance from different perspectives</p>
-                <hr />
-                <p>This application consists of displaying a dashboard from data provided by a back end built with Spring Boot.</p>
-                <Link className="btn btn-primary btn-lg" to="/dashboard">
-                    Access dashboard
-                </Link>
+            <Navbar />
+            <div className="container">
+                <div className="jumbotron">
+                    <h1 className="display-4">Sales Dash</h1>
+                    <p className="lead">Analyze your sales performance from different perspectives</p>
+                    <hr />
+                    <p>This application consists of displaying a dashboard from data provided by a back end built with Spring Boot.</p>
+                    <Link className="btn btn-primary btn-lg" to="/dashboard">
+                        Access dashboard
+                    </Link>
+                </div>
             </div>
-        </div>
-        <Footer />
+            <Footer />
         </>
     );
 }
