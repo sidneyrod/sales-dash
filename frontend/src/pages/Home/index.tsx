2e8894e99c0022e71d6db2ef59daab4b6631e706
Sidney Rodrigues <sidney.silva.rodrigues@outlook.com>
2021-05-07 11:02:12 -0300

    Home, navigation

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,8 +1,24 @@
+import Footer from "components/Footer";
+import Navbar from "components/Navbar";
+import { Link } from "react-router-dom";
+
 const Home = () => {
     return (
-        <div>
-            <h1>Home page</h1>
+        <>
+        <Navbar />
+        <div className="container">
+            <div className="jumbotron">
+                <h1 className="display-4">Sales Dash</h1>
+                <p className="lead">Analyze your sales performance from different perspectives</p>
+                <hr />
+                <p>This application consists of displaying a dashboard from data provided by a back end built with Spring Boot.</p>
+                <Link className="btn btn-primary btn-lg" to="/dashboard">
+                    Access dashboard
+                </Link>
+            </div>
         </div>
+        <Footer />
+        </>
     );
 }
 
